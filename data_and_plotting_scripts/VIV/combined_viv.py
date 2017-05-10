a95c40e84650811071d4e718f9022f06f4c327bb
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2017-05-09 17:40:35 -0700

    paper edits

--- data_and_plotting_scripts/VIV/combined_viv.py
+++ data_and_plotting_scripts/VIV/combined_viv.py
@@ -14,35 +14,34 @@ from scipy import signal
 
 def main():
 
-	print "-"*80
-	print "Making validaiton plot for an oscillating cylinder in flow."
-	print "-"*80
-	caseFolder = '/scratch/src/cuIBM/validation/osc/VIV'
-	name = '/scratch/src/cuIBM/validation/osc/VIV/Ured'
-	fileid = '/midPosition'
+    print("-"*80)
+    print("Making validaiton plot for an oscillating cylinder in flow.")
+    print("-"*80)
+    caseFolder = '/scratch/src/cuIBM/validation/osc/VIV'
+    name = '/scratch/src/cuIBM/validation/osc/VIV/Ured'
+    fileid = '/midPosition'
 
-	ys = [0.0714, 0.5616, 0.5234, 0.451, 0.371, 0.0696]
-	yc = [0.0714, 0.5286, 0.4894, 0.435, 0.381, 0.0696]
-	em_lc = [0.07161494006079072,0.5717792720930622,0.5344207235887237, 0.46769951947874355, 0.38874337573979395, 0.14012940247913452] #data output from embedded loose coupling
-	em_sc = [0.07406970331657958, 0.5443322122816592, 0.48226014304124076, 0.3941003346010647, 0.310016601470416, 0.0881615228777407] #data output from embeded strong coupling
-	ex_lc = [0.07041599084467354, 0.5675364513644794, 0.5202419621998599, 0.4533280558268404, 0.38804988964561826, 0.12004663709542096] #data output from external loose coupling
-	ex_sc = [0.0742857142857144, 0.5599999999999999, 0.5069387755102042, 0.4342857142857142, 0.35346938775510206, 0.10448979591836749] #data output from external strong coupling
-	x = [3, 4, 5, 6, 7, 8]
+    ys = [0.0714, 0.5616, 0.5234, 0.451, 0.371, 0.0696]
+    yc = [0.0714, 0.5286, 0.4894, 0.435, 0.381, 0.0696]
+    em_lc = [0.07161494006079072,0.5717792720930622,0.5344207235887237, 0.46769951947874355, 0.38874337573979395, 0.14012940247913452] #data output from embedded loose coupling
+    em_sc = [0.07406970331657958, 0.5443322122816592, 0.48226014304124076, 0.3941003346010647, 0.310016601470416, 0.0881615228777407] #data output from embeded strong coupling
+    ex_lc = [0.07041599084467354, 0.5675364513644794, 0.5202419621998599, 0.4533280558268404, 0.38804988964561826, 0.12004663709542096] #data output from external loose coupling
+    ex_sc = [0.0742857142857144, 0.5599999999999999, 0.5069387755102042, 0.4342857142857142, 0.35346938775510206, 0.10448979591836749] #data output from external strong coupling
+    x = [3, 4, 5, 6, 7, 8]
 
-	plt.plot(x,ys,'sk',label='Ann & Kallindens (2006)')
-	plt.plot(x,yc,'ok',label='Borazjani et al. (2008)')
-	plt.plot(x,ex_lc,'^b',label='External loose')
-	plt.plot(x,ex_sc,'db',label='External strong')
-	plt.plot(x,em_lc,'xg',label='Embedded loose')
-	plt.plot(x,em_sc,'+g',label='Embedded strong')
-	plt.xlabel('Ured')
-	plt.ylabel('Maximum Amplitude')
-	plt.xlim([2,9])
-	plt.ylim([0,0.6])
-	plt.legend(loc='best', fancybox=True)
-	plt.savefig('VIV_combine.pdf')
-	plt.clf()
+    plt.plot(x, ys, 'sk', label='Ann & Kallindens (2006)')
+    plt.plot(x, yc, 'ok', label='Borazjani et al. (2008)')
+    plt.plot(x, ex_lc, '^b', label='External loose')
+    plt.plot(x, ex_sc, 'db', label='External strong')
+    plt.plot(x, em_lc, 'xg', label='Embedded loose')
+    plt.plot(x, em_sc, '+g', label='Embedded strong')
+    plt.xlabel('Ured')
+    plt.ylabel('Maximum Amplitude')
+    plt.xlim([2, 9])
+    plt.ylim([0, 0.6])
+    plt.legend(loc='best', numpoints=1)
+    plt.savefig('VIV_combine.pdf')
+    plt.clf()
 
 #run
 main()
-
